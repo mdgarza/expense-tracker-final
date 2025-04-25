b5534883d2eca590d8504bd85011f4861b0b5678
Matthew Garza <raijox989@outlook.com>
2025-04-25 19:34:57 -0400

    Connected Railway MySQL

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,13 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    port: process.env.DB_PORT || 3306,
+const sequelize = new Sequelize(process.env.DB_URL, {
+  dialect: 'mysql',
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: true
+    }
   }
-);
+});
 
 module.exports = sequelize;
