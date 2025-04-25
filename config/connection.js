c5616eada2007bb2d9dea37b7b916655ba615aa7
Matthew Garza <raijox989@outlook.com>
2025-04-25 19:40:18 -0400

    Connected Railway MySQL

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
-const Sequelize = require('sequelize');
 require('dotenv').config();
+const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_URL, {
   dialect: 'mysql',
   dialectOptions: {
     ssl: {
-      rejectUnauthorized: true
+      rejectUnauthorized: false
     }
   }
 });
